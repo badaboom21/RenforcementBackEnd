71d7d850ab6d25858ef95ad9d1de5aaf72ea1946
badaboom21 <130473771+badaboom21@users.noreply.github.com>
2026-04-23 16:36:07 +0200

    fetch comment

--- assurmobile/assurmobile/hooks/fetchData.tsx
+++ assurmobile/assurmobile/hooks/fetchData.tsx
@@ -7,8 +7,6 @@ type Headers = {
   Authorization?: string;
 };
 
-console.log("API_BASE_URL", process.env.API_BASE_URL);
-
 const API_BASE_URL_CONST =
   process.env.API_BASE_URL || "http://localhost:3000/api";
 
